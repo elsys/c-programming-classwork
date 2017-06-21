42c18a006513fa328afe61eaea9cf8149a7b3f12
Alexander Chernaev <alchernaev@gmail.com>
2017-06-22 00:35:50 +0300

    Added 28 lesson files

---
+++ 28_Data_strutures/A/calendar.c
@@ -0,0 +1,12 @@
+struct calendar_t {
+    int events_count[52];
+};
+
+struct events_month_t {
+    int events[13];
+};
+struct events_month_t events_per_month(struct calendar_t);
+
+int main() {
+    
+}
